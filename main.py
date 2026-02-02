fe221bde23e0efbc6058a9d4064a5397d6be16c1
Quang Hy Le <Lequanghy1998@yahoo.ca>
2026-02-01 19:46:43 -0500

    feat: update allow origin

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   
         "http://127.0.0.1:5173",
-        "https://pdf-utility-9tma.onrender.com/",
+        "https://pdf-utility-api-88dm.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],           # allow POST, GET, etc.
